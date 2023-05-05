f3631fa118d64220f55981d903df559a71feea1a
Flying-Angels <93036139+Flying-Angels@users.noreply.github.com>
2023-05-05 09:57:00 +0800

    4
    语义分析

--- exprparse.cpp
+++ exprparse.cpp
@@ -9,6 +9,7 @@
 class exprparse{
     vector<token> tokens;
     int pos;
+    int result;
 public:
     exprparse(vector<token> tokens): tokens(std::move(tokens)), pos(0){}
 
@@ -38,7 +39,7 @@ public:
             }else if (token.type == ident){
                 cout << token.value;
             }else{
-                cout << wordlist_str[token.type] << " ";
+                cout << wordlist_str[token.type];
             }
         }
     }
@@ -50,37 +51,65 @@ public:
 
     // expression 表达式的产生式
     // <表达式> → [+|-]<项>{<加减运算符><项>}
-    void parse_expression(){
+    int parse_expression(){
+        int result = 0;
+        bool is_negative = false;
         if(peektoken().type == plussym || peektoken().type == minussym){
+            if(peektoken().type == minussym)
+                is_negative = true;
             nexttoken();
+        }else{
+            result = parse_term();
         }
-        parse_term();
+
         while(peektoken().type == plussym || peektoken().type == minussym){
-            nexttoken();
-            parse_term();
+            if(peektoken().type == plussym){
+                nexttoken();
+                result += parse_term();
+            }else{
+                nexttoken();
+                result -= parse_term();
+            }
+            
         }
+        return is_negative?-result:result;
     }
 
     // term 项的产生式
     // <项> → <因子>{<乘除运算符><因子>}
-    void parse_term(){
-        parse_factor();
+    int parse_term(){
+        int result = parse_factor();
         while(peektoken().type == times || peektoken().type == slash){
-            nexttoken();
-            parse_factor();
+            if(peektoken().type == times){
+                nexttoken();
+                result *= parse_factor();
+            }else{
+                nexttoken();
+                int divisor = parse_factor();
+                if(divisor == 0){
+                    throw std::runtime_error("0"); // Division by zero error
+                    return 0;
+                }
+                result /= divisor;
+            }
         }
+
+        return result;
     }
 
     // factor 因子的产生式
     // <因子> → <标识符>|<无符号整数>|'('<表达式>')'
-    void parse_factor(){
+    int parse_factor(){
+        int result = 0;
+        auto p = peektoken();
         if(peektoken().type == ident){
             nexttoken();
         }else if(peektoken().type == number){
+            result = peektoken().num;
             nexttoken();
         }else if(peektoken().type == lparen){
             nexttoken();
-            parse_expression();
+            result = parse_expression();
             if(peektoken().type == rparen){
                 nexttoken();
             }else{
@@ -89,13 +118,14 @@ public:
         }else{
             throw std::runtime_error(parserErrors[23]);
         }
-
+        return result;
     }
 
     void end(){
         if(pos<tokens.size())
             throw std::runtime_error(parserErrors[8]+" expression already ended before token "+std::to_string(pos) + " [ " + tokens[pos].show() + " ]");
     }
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include "lexer.cpp"
-#include "exprparse.cpp"
+
 using namespace std;
 
 int main(int argc,char *argv[]){
@@ -8,50 +8,39 @@ int main(int argc,char *argv[]){
         cout<<"Usage: main [filename]"<<endl;
         return 0;
     }
-    string fn = "tests/5";
+    string fn = "tests/1";
     if(argc==2){
         fn = argv[1];
     }
     cout<< "File: " << fn << endl;
     ifstream fin(fn);
-    if (!fin)
+    auto tokens = lexer(fin);
+    unordered_map<string, int> ident_cnt;
+    for (auto &item : tokens)
     {
-        cerr << "Error opening file." << endl;
-        return 0;
+        if (item.type == ident)
+        {
+            ident_cnt[item.value]++;
+        }
     }
-    auto tokens = lexer(fin,fn);
-//    /*
-//    LAB 1
-//    */
-//    unordered_map<string, int> ident_cnt;
-//    for (auto &item : tokens)
-//        if (item.type == ident)
-//            ident_cnt[item.value]++;
-//    for (auto &item : ident_cnt)
-//        if (!wordlist_map.count(item.first))
-//            cout << "(" << item.first << ": " << item.second << ")" << endl;
-//    cout<<endl<<endl;
-//    /*
-//    LAB 2
-//    */
-//    for (auto &item : tokens)
-//        cout << item << endl;
-//    save_token_list(tokens, fn+".token");
-     //auto tokens = parse_token_file(fn);
-/*
-LAB 3
-*/
-
-    auto expression = exprparse(tokens);
-    expression.showraw();
+    for (auto &item : ident_cnt)
+    {
+        if (!wordlist_map.count(item.first))
+        {
+            cout << "(" << item.first << ": " << item.second << ")" << endl;
+        }
+    }
     cout<<endl<<endl;
-    try{
-        expression.parse_expression();
-        expression.end();
-        cout<<"Passed"<<endl;
-    }catch (const std::exception& e){
-        cout << "Error: " << e.what() << endl;
+
+    for (auto &item : tokens)
+    {
+        cout << item << endl;
     }
-    return 0;
+    save_token_list(tokens, fn+".token");
+    // auto testdic = parse_token_file(fn+".token");
+    // for (auto &item : testdic)
+    // {
+    //     cout << item << endl;
+    // }
 
 }
